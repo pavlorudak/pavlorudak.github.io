a9e7f83afc34db3be622bcb49ebe9120fb974bb3
Pavlo Rudak <112572097+pavlorudak@users.noreply.github.com>
2023-08-02 12:56:26 +0000

    new

--- rudak-project/src/App.js
+++ rudak-project/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rudak Projects
+          Rudak Projects 
+          Portfolio
         </p>
         
       </header>
