1f18afda2cc6a77a2a7a7bc655ec7878e28d7b9a
Pavlo Rudak <112572097+pavlorudak@users.noreply.github.com>
2023-08-02 13:07:21 +0000

    Nav

--- rudak-project/src/App.js
+++ rudak-project/src/App.js
@@ -1,19 +1,31 @@
 import logo from './logo.svg';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Rudak Projects 
-          Portfolio V. 1.0
-        </p>
-        
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
